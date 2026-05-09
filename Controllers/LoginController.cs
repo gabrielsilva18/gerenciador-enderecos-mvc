f3153a34434b77df338bd66523fb85632f828764
Gabriel <gabriellealmaio1@gmail.com>
2026-05-09 00:17:57 -0300

    feat: implementa login e cadastro de usuários

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GerenciadorEnderecos.Data;
+using GerenciadorEnderecos.Models;
 
 namespace GerenciadorEnderecos.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public LoginController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ Controllers/RegistroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorEnderecos.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
